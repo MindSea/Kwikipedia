1e7e614cdc73dbd2235c11bfa7424fde51699fcc
Gavin Anderegg <gavin@anderegg.ca>
2013-11-15 21:18:07 -0400

    Starting wiki

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
-var wikiModule = angular.module('wiki', []);
+var wikiModule = angular.module('wiki', ['ngRoute']);
+
+wikiModule.config(function($locationProvider, $routeProvider) {
+	$locationProvider.html5Mode(false); // For ease of demo-ing
+	
+	$routeProvider.when('/:slug?', {
+		controller: 'contentController',
+		templateUrl: 'content.html'
+	});
+});
 
 wikiModule.controller('wikiController', function($scope) {
-	$scope.list = [
-		{'text': 'Some', 'color': 'red'},
-		{'text': 'neat', 'color': 'blue'},
-		{'text': 'things!', 'color': 'green'}
-	];
+	
+});
+
+wikiModule.controller('contentController', function($scope, $routeParams) {
+	$scope.slug = $routeParams.slug;
 });
